b883631f2c09cc369d7d1a72af7c1975dcd4b67a
cubos-d <alejandro4cm@gmail.com>
2026-07-03 14:41:24 -0500

    feat: at last. Installed the vscode theme

--- users/cubos/vscode/vscode.nix
+++ users/cubos/vscode/vscode.nix
@@ -6,9 +6,16 @@
     # This makes it easy to add extensions without manual installation
     profiles = {
       default = {
-        extensions = with pkgs.vscode-extensions; [
-          bbenoist.nix
-          ms-python.python
+        extensions = with pkgs; [
+          vscode-extensions.bbenoist.nix
+          vscode-extensions.ms-python.python
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "amethyst-theme";
+            publisher = "amodio";
+            version = "2025.1.1516";
+            sha256 = "sha256-tqJ0N4lEG+4OV1ZqyUrL/0OD/eM9uXmqSAdWYlpauDU=";
+          }
         ];
         userSettings = {
           "workbench.colorTheme" = "Amethyst Dark (Higher Contrast)";
