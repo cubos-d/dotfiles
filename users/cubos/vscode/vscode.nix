a9ad2aba2f1c148be637431f7460d06da20946e0
cubos-d <alejandro4cm@gmail.com>
2026-07-03 14:24:56 -0500

    feat: organized vscode settings

--- users/cubos/vscode/vscode.nix
+++ users/cubos/vscode/vscode.nix
@@ -4,16 +4,20 @@
   programs.vscode = {
     enable = true;
     # This makes it easy to add extensions without manual installation
-    extensions = with pkgs.vscode-extensions; [
-      bbenoist.nix
-      ms-python.python
-    ];
-    userSettings = {
-      "workbench.colorTheme" = "Amethyst Dark (Higher Contrast)";
-      "editor.fontSize" = 14;
-      "editor.fontFamily" = "ComicShannsMono Nerd Font";
-      "terminal.integrated.fontFamily" = "ComicShannsMono Nerd Font";
-      "terminal.integrated.fontSize" = 14;
+    profiles = {
+      default = {
+        extensions = with pkgs.vscode-extensions; [
+          bbenoist.nix
+          ms-python.python
+        ];
+        userSettings = {
+          "workbench.colorTheme" = "Amethyst Dark (Higher Contrast)";
+          "editor.fontSize" = 14;
+          "editor.fontFamily" = "ComicShannsMono Nerd Font";
+          "terminal.integrated.fontFamily" = "ComicShannsMono Nerd Font";
+          "terminal.integrated.fontSize" = 14;
+        };
+      };
     };
   };
 }
