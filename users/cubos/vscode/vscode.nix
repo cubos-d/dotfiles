fe19763bb4d1cdc811aefbbbbdd57695d3e05154
cubos-d <alejandro4cm@gmail.com>
2026-07-03 20:01:08 -0500

    feat: Development environments and shell.nix envs done

--- users/cubos/vscode/vscode.nix
+++ users/cubos/vscode/vscode.nix
@@ -9,6 +9,20 @@
         extensions = with pkgs; [
           vscode-extensions.bbenoist.nix
           vscode-extensions.ms-python.python
+          vscode-extensions.ms-python.vscode-pylance
+          vscode-extensions.ms-vscode.cmake-tools
+          vscode-extensions.ms-vscode.cpptools
+          vscode-extensions.ms-vscode.cpptools-extension-pack
+          vscode-extensions.ms-vscode.makefile-tools
+          vscode-extensions.ms-toolsai.jupyter
+          vscode-extensions.ms-toolsai.jupyter-keymap
+          vscode-extensions.ms-toolsai.jupyter-renderers
+          vscode-extensions.ms-toolsai.vscode-jupyter-cell-tags
+          vscode-extensions.ms-toolsai.vscode-jupyter-slideshow
+          vscode-extensions.rust-lang.rust-analyzer
+          vscode-extensions.llvm-vs-code-extensions.vscode-clangd
+          vscode-extensions.continue.continue
+          vscode-extensions.davidanson.vscode-markdownlint
         ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
           {
             name = "amethyst-theme";
@@ -23,6 +37,8 @@
           "editor.fontFamily" = "ComicShannsMono Nerd Font";
           "terminal.integrated.fontFamily" = "ComicShannsMono Nerd Font";
           "terminal.integrated.fontSize" = 14;
+          "editor.tabSize" = 4;
+          "editor.insertSpaces" = true;
         };
       };
     };
