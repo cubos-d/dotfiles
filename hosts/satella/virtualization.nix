50daae0a8059b28ee13501fbbb375583ce7b0d54
cubos-d <ja.cub05.19@gmail.com>
2026-07-15 09:54:01 -0500

    feat: For nox we will work only with qemu. I will worry later for virt-manager

--- hosts/satella/virtualization.nix
+++ hosts/satella/virtualization.nix
@@ -3,22 +3,13 @@
 {
   # 1. Enable KVM kernel modules
   boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
-
-  # 2. Add your user to the necessary groups (replace "YOUR_USERNAME")
-  users.users.cubos.extraGroups = [ "libvirtd" "kvm" ];
-
-  # 3. Enable virtualization services
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      swtpm.enable = true;
-    };
-  };
-
+  boot.binfmt.emulatedSystems = [
+    "aarch64-linux"
+    "riscv64-linux"
+  ];
+  systemd.tmpfiles.rules = [ "L+ /var/lib/qemu/firmware - - - - ${pkgs.qemu}/share/qemu/firmware" ];
   # 4. Install virt-manager (GUI) and other QEMU tools
   environment.systemPackages = with pkgs; [
-    virt-manager
-    virt-viewer
     qemu
   ];
 }
