cd4f742324d08e0bf209693aec8ab68e1ced9e71
cubos-d <ja.cub05.19@gmail.com>
2026-08-27 11:57:58 -0500

    feat: Neovim is almost like a IDE. Tree and UI stuff already working

--- users/cubos/nvim/nvf.nix
+++ users/cubos/nvim/nvf.nix
@@ -34,6 +34,45 @@
           style = "storm";
           transparent = true;
         };
+        filetree = {
+          neo-tree = {
+            enable = true;
+            setupOpts = {
+              window = {
+                position = "left"; 
+                width = 30;
+              };
+            };
+          };
+        };
+        tabline = {
+          nvimBufferline = {
+            enable = true;
+          };
+        };
+        maps.normal = {
+          "<C-b>" = {
+            action = ":Neotree toggle<CR>";
+            silent = true;
+            desc = "Toggle File Explorer Sidebar";
+          };
+          "<M-1>" = { action = "<Cmd>BufferLineGoToBuffer 1<CR>"; silent = true; desc = "Go to tab 1"; };
+          "<M-2>" = { action = "<Cmd>BufferLineGoToBuffer 2<CR>"; silent = true; desc = "Go to tab 2"; };
+          "<M-3>" = { action = "<Cmd>BufferLineGoToBuffer 3<CR>"; silent = true; desc = "Go to tab 3"; };
+          "<M-4>" = { action = "<Cmd>BufferLineGoToBuffer 4<CR>"; silent = true; desc = "Go to tab 4"; };
+          "<M-5>" = { action = "<Cmd>BufferLineGoToBuffer 5<CR>"; silent = true; desc = "Go to tab 5"; };
+          "<M-6>" = { action = "<Cmd>BufferLineGoToBuffer 6<CR>"; silent = true; desc = "Go to tab 6"; };
+          "<M-7>" = { action = "<Cmd>BufferLineGoToBuffer 7<CR>"; silent = true; desc = "Go to tab 7"; };
+          "<M-8>" = { action = "<Cmd>BufferLineGoToBuffer 8<CR>"; silent = true; desc = "Go to tab 8"; };
+          "<M-9>" = { action = "<Cmd>BufferLineGoToBuffer 9<CR>"; silent = true; desc = "Go to tab 9"; };
+          "<M-Right>" = { action = "<Cmd>BufferLineCycleNext<CR>"; silent = true; desc = "Next tab"; };
+          "<M-Left>"  = { action = "<Cmd>BufferLineCyclePrev<CR>"; silent = true; desc = "Previous tab"; };
+          "<M-w>" = { 
+            action = "<Cmd>bp | bd #<CR>"; 
+            silent = true; 
+            desc = "Close current tab cleanly"; 
+          };
+        };
         languages = {
           enableTreesitter = true;
           clang = {
@@ -85,6 +124,18 @@
         statusline.lualine.enable = true;
         telescope.enable = true;
         autocomplete.nvim-cmp.enable = true;
+
+        luaConfigPost = ''
+          vim.api.nvim_create_autocmd("VimEnter", {
+            callback = function()
+              -- 1. Open the file tree
+              vim.cmd("Neotree show")
+              -- 2. Optional: Shift focus back to the file you opened
+              -- Without this line, your cursor will stay inside the file tree sidebar
+              vim.cmd("wincmd p") 
+            end
+          })
+        '';
       };
     };
   };
