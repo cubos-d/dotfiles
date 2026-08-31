6d526ed82db0818efcbfe4f33256b583e14d7a10
cubos-d <ja.cub05.19@gmail.com>
2026-08-31 18:21:57 -0500

    feat: Added vim as a global package again, as it may be needed for root user

--- hosts/satella/configuration.nix
+++ hosts/satella/configuration.nix
@@ -81,6 +81,7 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
+    vim
     curl
     wget
     rocmPackages.rocminfo
